a63e41e92f1b7d8c495cb4a0fa0dc2213b507980
Karen Vardanian <vkaren1777@icloud.com>
2026-04-15 10:38:12 +0300

    refactor

--- index.ts
+++ index.ts
@@ -1,25 +1,20 @@
 import TelegramBot from "node-telegram-bot-api";
-import dotenv from "dotenv";
-import express from "express";
-dotenv.config();
+import { config } from "dotenv";
+config();
 
-const app = express();
+
 const TGBot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN as string, { polling: true });
 
 
-TGBot.on("message", async msg => {
-  const chatId = msg.chat.id;
-  const msgId = msg.message_id;
-  const msgText = msg.text;
-  if ((msgText && /т+ю+/i.test(msgText)) || (msgText && /t+u+/i.test(msgText))) {
-    return await TGBot.deleteMessage(chatId, msgId);
-  }
-})
+const startTelegramBot = () => {
+    TGBot.on("message", async msg => {
+        const chatId = msg.chat.id;
+        const msgId = msg.message_id;
+        const msgText = msg.text;
+        if ((msgText && /т+ю+/i.test(msgText)) || (msgText && /t+u+/i.test(msgText))) {
+            return await TGBot.deleteMessage(chatId, msgId);
+        }
+    })
+}
 
-app.get("/", (req, res) => {
-  res.status(200).send("OK");
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+startTelegramBot();
