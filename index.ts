1ed5cb92a194f2781b7f90fdedbe59f895449fc6
Karen Vardanian <vkaren1777@icloud.com>
2026-04-15 10:18:44 +0300

    fixed deployment for server

--- index.ts
+++ index.ts
@@ -6,15 +6,16 @@ config();
 const TGBot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN as string, { polling: true });
 
 
-const startTelegramBot = () => {
-    TGBot.on("message", async msg => {
-        const chatId = msg.chat.id;
-        const msgId = msg.message_id;
-        const msgText = msg.text;
-        if ((msgText && /т+ю+/i.test(msgText)) || (msgText && /t+u+/i.test(msgText))) {
-            return await TGBot.deleteMessage(chatId, msgId);
-        }
-    })
+TGBot.on("message", async msg => {
+    const chatId = msg.chat.id;
+    const msgId = msg.message_id;
+    const msgText = msg.text;
+    if ((msgText && /т+ю+/i.test(msgText)) || (msgText && /t+u+/i.test(msgText))) {
+        return await TGBot.deleteMessage(chatId, msgId);
+    }
+})
+
+export default function handler(req: Request, res: any) {
+    res.status(200).send("Bot is running");
 }
 
-startTelegramBot();
